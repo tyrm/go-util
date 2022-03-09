72eb34520e3cadf7e1a3c3368a2e6a347484838e
Tyr Mactire <tyr@pettingzoo.co>
2022-03-08 21:49:42 -0800

    compile template

---
+++ pkgerutil/compile_templates.go
@@ -0,0 +1,54 @@
+package pkgerutil
+
+import (
+	"github.com/markbates/pkger"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+// CompileTemplates takes a pkger dir and turns it into templates.
+// Note: dir should be provided by pkger.Include
+func CompileTemplates(dir string, suffix string) (*template.Template, error) {
+	tpl := template.New("")
+
+	tpl.Funcs(template.FuncMap{
+		"dec": func(i int) int {
+			i--
+			return i
+		},
+		"htmlSafe": func(html string) template.HTML {
+			return template.HTML(html)
+		},
+		"inc": func(i int) int {
+			i++
+			return i
+		},
+	})
+
+	err := pkger.Walk(dir, func(path string, info os.FileInfo, _ error) error {
+		if info.IsDir() || !strings.HasSuffix(path, suffix) {
+			return nil
+		}
+		f, err := pkger.Open(path)
+		if err != nil {
+			return err
+		}
+
+		// Now read it.
+		sl, err := ioutil.ReadAll(f)
+		if err != nil {
+		}
+
+		// It can now be parsed as a string.
+		_, err = tpl.Parse(string(sl))
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	return tpl, err
+}
---
+++ pkgerutil/compile_templates_test.go
@@ -0,0 +1,52 @@
+package pkgerutil
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
+
+func TestCompileTemplates(t *testing.T) {
+	templates, err := CompileTemplates("", ".gohtml")
+	if err != nil {
+		t.Errorf("compiling templates: %s", err.Error())
+		return
+	}
+
+	tables := []struct {
+		// inputs
+		template string
+		input    interface{}
+
+		// outputs
+		output string
+		err    error
+	}{
+		{"static", nil, "Static Text.", nil},
+		{"dynamic", "Dynamic Text.", "Dynamic Text.", nil},
+		{"dec", 5, "4", nil},
+		{"inc", 1, "2", nil},
+	}
+
+	for i, table := range tables {
+		i := i
+		table := table
+
+		name := fmt.Sprintf("[%d] Executing template %s", i, table.template)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			var resp bytes.Buffer
+			err := templates.ExecuteTemplate(&resp, table.template, table.input)
+			if err != table.err {
+				t.Errorf("[%d] template execution error, got: '%v', want: '%v'", i, err, table.err)
+				return
+			}
+
+			respStr := resp.String()
+			if respStr != table.output {
+				t.Errorf("[%d] got invalid output for %s, got: '%v', want: '%v'", i, table.template, respStr, table.output)
+			}
+		})
+	}
+}
