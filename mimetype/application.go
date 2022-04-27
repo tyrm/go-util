3f768a51c551115195a0512939b0ffed3a850c0f
Tyr Mactire <tyr@pettingzoo.co>
2022-04-26 20:05:16 -0700

    add mime types (#3)

---
+++ mimetype/application.go
@@ -0,0 +1,12 @@
+package mimetype
+
+const (
+	// ApplicationActivityJSON represents an activity json object
+	ApplicationActivityJSON = `application/activity+json`
+	// ApplicationLDJSONActivityStreams represents an activity stream json object
+	ApplicationLDJSONActivityStreams = `application/ld+json; profile="https://www.w3.org/ns/activitystreams"`
+	// ApplicationJRDJSON represents a json resource descriptor
+	ApplicationJRDJSON = `application/jrd+json`
+	// ApplicationJSON represents a json object
+	ApplicationJSON = `application/json`
+)
---
+++ mimetype/text.go
@@ -0,0 +1,6 @@
+package mimetype
+
+const (
+	// TextHTML represents  html formatted text
+	TextHTML = `text/html`
+)
