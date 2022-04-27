484cb05293dbf2c049e2f23d55ffbb67a3ca7051
Tyr Mactire <tyr@pettingzoo.co>
2022-04-26 21:09:58 -0700

    add text/plain

--- mimetype/text.go
+++ mimetype/text.go
@@ -1,6 +1,8 @@
 package mimetype
 
 const (
-	// TextHTML represents  html formatted text
+	// TextHTML represents html formatted text
 	TextHTML = `text/html`
+	// TextPlain represents unformatted text
+	TextPlain = `text/plain`
 )
